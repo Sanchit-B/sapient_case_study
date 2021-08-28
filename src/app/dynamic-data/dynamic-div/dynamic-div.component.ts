75e5caea0e15030dccef19f80d011b52d1d0d148
Sanchit Bansal <sanchitb007@gmail.com>
2021-08-28 13:09:49 +0530

    Dynamic divs on scroll event

--- src/app/dynamic-data/dynamic-div/dynamic-div.component.ts
+++ src/app/dynamic-data/dynamic-div/dynamic-div.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dynamic-div',
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DynamicDivComponent implements OnInit {
 
+  dynamicDivs = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.appendDivs(0, 10);
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  public onScrollingEvent(): void {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      let start = this.dynamicDivs.length;
+      this.appendDivs(start, start+10);      
+    }
+  }
+
+  appendDivs(start, end) {
+    for(let i=start; i<end; i++) {
+      this.dynamicDivs.push(i);
+    }
+  }
+
+  showAlert(divcount) {
+    alert(`Button  in div ${divcount} clicked`)
   }
 
 }
